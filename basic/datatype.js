35174830e2f0ce8df43bd77212b2db18fcb13ef6
utkarshamande <utkarshamande4@gmail.com>
2025-05-12 06:02:03 +0000

    container confo done

---
+++ basic/datatype.js
@@ -0,0 +1,26 @@
+"use strict";//treal all js code as newer version 
+//alert("hello")
+//alert(3+3)// we are using nodejs ,not browser 
+console.log(3+3) // code readability should be high
+console.log("utkarsha")
+
+let name ="utkarsha"
+let age= 23
+let isloggedIn =false
+
+//number => 2 to power 53
+//bigint
+//string =>"uvbm"
+//boolean => true/flase
+//null => standalone value (null value)
+//undefined => value not assign
+//symbol => unique
+
+//object => 
+
+console.log(typeof age);
+console.table([name,age,isloggedIn])
+
+
+console.log(typeof null)// null type is object imp que 
+console.log(typeof undefined)
